3be4c6f818584a8ccabd16df3d39fb0a9f53ae05
Jamie Herron <jnherron3@gmail.com>
2025-09-11 13:59:49 -0600

    Linear Regression
    Created BikeShareAnalysis.R
    Performed predictive linear regression
    Created Kaggle Submission LinearPreds.csv
    Updated README

---
+++ BikeShareAnalysis.R
@@ -0,0 +1,24 @@
+# Linear Regression ---------------------------
+lin_mod <- linear_reg() %>%
+  set_engine("lm") %>%
+  set_mode("regression") %>%
+  fit(formula = log(count) ~ season + holiday + workingday + weather + temp + atemp + humidity + windspeed, data=train_data)
+
+# Generate Predictions
+preds <- exp(predict(lin_mod, new_data=test_data))
+preds
+
+# Format the Predictions for Submission to Kaggle
+kag_sub <- preds %>%
+  bind_cols(., test_data) %>%
+  select(datetime, .pred) %>%
+  rename(count=.pred) %>%
+  mutate(count=pmax(0, count)) %>%
+  mutate(datetime=as.character(format(datetime)))
+
+# Write out the file
+vroom_write(x=kag_sub, file="./LinearPreds.csv", delim=",")
+
+
+
+
--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # KaggleBikeShare
-Code for Kaggle Bike Share Comptetition
+Code for Kaggle Bike Share Competition
 
 BikeShare.R reads in the tidyverse and tidymodels libraries.
+BikeShareEDA.R reads in the data and provides some EDA, including plots.
+BikeShareAnalysis.R applies predictive linear regression.
